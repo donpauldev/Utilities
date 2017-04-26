3c09e8ef0d95f5e27366fbb8c42aa4ad45021ac9
Don Paul <donpaulpm92@gmail.com>
2017-04-26 15:34:35 +0530

    Update and rename countryCodeDictionary.swift to CountryCodeDictionary.swift

--- CountryCodeDictionary.swift
+++ CountryCodeDictionary.swift
@@ -1,6 +1,6 @@
 //
-//  Utilities.swift
-//  Utilities Swift
+//  CountryCodeDictionary.swift
+//  CountryCodeDictionary Swift
 //
 //  Created by Don Paul on 06/03/16.
 //  Copyright © 2016. All rights reserved.
