1ed5b85181608919ceffb399041bdf4adf21cf51
Don Paul <donpaulpm92@gmail.com>
2017-07-12 18:57:15 +0530

    Utilities Framework
    Utilities boilerplate framework

---
+++ Utilities.framework/Headers/Utilities.h
@@ -0,0 +1,19 @@
+//
+//  Utilities.h
+//  Utilities
+//
+//  Created by AJESHKUMAR CHEMPANKULAM DIVAKARAN on 12/07/17.
+//  Copyright © 2017 DonPaulPM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Utilities.
+FOUNDATION_EXPORT double UtilitiesVersionNumber;
+
+//! Project version string for Utilities.
+FOUNDATION_EXPORT const unsigned char UtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Utilities/PublicHeader.h>
+
+
